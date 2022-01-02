2a048ea95329d71399578884e58415cac102826e
unknown <cdwenz@gmail.com>
2022-01-02 20:48:59 -0300

    search

--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
-import { Avatar } from 'react-native-elements';
+import React from "react";
+import { View, Image, StyleSheet, Text } from "react-native";
+import { Avatar } from "react-native-elements";
 
-const ImageScreen = ({route}) => {
-    const { image } = route.params;
-    return (
-        <View>
-            <Image source={{uri: image.src.medium, height:350}}/>
-            <View>
-                <Avatar></Avatar>
-                <Text>{image.photographer}</Text>
-            </View>
-        </View>
-    )
-}
+const ImageScreen = ({ route }) => {
+  const { image } = route.params;
+  return (
+    <View>
+      <Image source={{ uri: image.src.medium, height: 350 }} />
+      <View style={{ display: "flex", flexDirection: "row" }}>
+        <Avatar
+          title={image.photographer
+            .split(" ")
+            .map((e) => e[0])
+            .join("")
+            .toUpperCase()}
+          containerStyle={{ backgroundColor: "red" }}
+          rounded
+        />
+        <Text>{image.photographer}</Text>
+      </View>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({});
 
-})
-
-export default ImageScreen
+export default ImageScreen;
