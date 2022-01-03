2b28f849b113007e8bd82ee68e34bb376a41b9f3
unknown <cdwenz@gmail.com>
2022-01-02 21:17:37 -0300

    detail

--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -1,34 +1,53 @@
 import React from "react";
 import { View, Image, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { Avatar } from "react-native-elements";
-import * as WebBrowser from 'expo-web-browser';
+import * as WebBrowser from "expo-web-browser";
 import { Button } from "react-native-elements/dist/buttons/Button";
+import ImageList from "../components/ImageList";
 
 const ImageScreen = ({ route }) => {
   const { image } = route.params;
 
   const handlePress = async () => {
     await WebBrowser.openBrowserAsync(image.photographer_url);
-  }
+  };
   return (
     <View style={styles.imageDetail}>
       <Image source={{ uri: image.src.medium, height: 350 }} />
-      <View style={{ display: "flex" }}>
-        <View style={{ display: "flex", flexDirection: "row", alignItems:'center' }}>
+      <View
+        style={{
+          display: "flex",
+          paddingVertical: 18,
+          justifyContent: "space-between",
+          flexDirection: "row",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
           <Avatar
             title={image.photographer
               .split(" ")
               .map((e) => e[0])
               .join("")
               .toUpperCase()}
-            containerStyle={{ backgroundColor: "#368497" }}
+            containerStyle={{ backgroundColor: "red" }}
             rounded
           />
           <TouchableOpacity onPress={handlePress}>
             <Text style={styles.textPhotographer}>{image.photographer}</Text>
           </TouchableOpacity>
         </View>
-        <Button title='Download'/>
+        <Button title="Download" buttonStyle={{backgroundColor: '#368497'}}/>
+      </View>
+      <View>
+          <ImageList />
       </View>
     </View>
   );
@@ -42,10 +61,10 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   textPhotographer: {
-      color: "#fff",
-      fontWeight: "bold",
-      marginStart: 5,
-      fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    marginStart: 5,
+    fontSize: 18,
   },
 });
 
